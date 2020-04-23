771c75ea04922ad09a02ba8ee69f37a14c29f8f9
mussersl <58488336+mussersl@users.noreply.github.com>
2020-04-23 16:07:58 -0400

    Added movement

---
+++ SpaceBrigade/Assets/__Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    //states: 0 = ground, 1 = air, 2 = interaction, 3 = menu
+    static public Player S;
+
+    [Header("Set Dynamically")]
+    float hsp = 0;
+    float vsp = 0;
+    int state = 0;
+
+    [Header("Set in Inspector")]
+    float maxHsp = 50;
+    float maxVsp = 50;
+    float walkSpeed = 10;
+    float jump = 25;
+    float grav = -1;
+
+    // Singleton time
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else Debug.LogError("Player.Awake() - attempted to assign second Player.S");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Triggered: " + other.gameObject.name);
+        print("State: " + state + ", Vsp: " + vsp);
+        GameObject go = other.gameObject;
+        if (other.tag == "Collision")
+        {
+            if (state == 1 && vsp < 0)
+            {
+                state = 0;
+                Vector3 pos = transform.position;
+                while (pos.y <= other.transform.position.y)
+                    pos.y--;
+                transform.position = pos;
+                vsp = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float xAxis = Input.GetAxis("Horizontal");
+        hsp = walkSpeed * xAxis;
+        if (state == 1)
+        {
+            if (Input.GetKeyUp(KeyCode.Z) && vsp >= 6)
+                vsp = 6;
+            vsp += grav;
+            if (vsp <= -maxVsp)
+                vsp = -maxVsp;
+        }
+        if (state == 0 && Input.GetKeyDown(KeyCode.Z))
+        {
+            vsp += jump;
+            state = 1;
+        }
+        Vector3 pos = transform.position;
+        pos.x += hsp * Time.deltaTime;
+        pos.y += vsp * Time.deltaTime;
+        transform.position = pos;
+	}
+}
